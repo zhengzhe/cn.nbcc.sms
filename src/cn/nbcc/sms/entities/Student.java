db6153a6a0fb0e4ef0b1535cf6e8719c4fb0eb5a
zhengzhe <zhe_zheng@126.com>
2013-06-07 20:43:06 +0800

    Honor and GuestStudent
    添加荣誉学生，旁听学生

--- src/cn/nbcc/sms/entities/Student.java
+++ src/cn/nbcc/sms/entities/Student.java
@@ -1,15 +1,48 @@
 package cn.nbcc.sms.entities;
+
+import java.util.ArrayList;
+
 /**
  * @author  zhengzhe
  * @2013-5-7
  */
+
+
 public class Student {
+	
+	enum Grade{A,B,C,D,F};
 	
 	String id;
 	String name;
 	//获得的现有学分
 	int credits;
 	
+
+	boolean isHornor;
+	boolean isGuest;
+	public boolean isGuest() {
+		return isGuest;
+	}
+
+	public void setGuest(boolean isGuest) {
+		this.isGuest = isGuest;
+	}
+
+	public boolean isHornor() {
+		return isHornor;
+	}
+
+	public void setHornor(boolean isHornor) {
+		this.isHornor = isHornor;
+	}
+
+
+	private ArrayList<Grade> grades = new ArrayList<Student.Grade>();
+	
+	
+	public void addGrade(Grade aGrade){
+		grades.add(aGrade);
+	}
 	
 	public Student(String id,String name) {
 		this.id = id;
@@ -38,6 +71,39 @@ public class Student {
 	}
 	public void setCredits(int credits) {
 		this.credits = credits;
+	}
+
+	
+	/**
+	 * 获得平均成绩
+	 * @return 返回平均成绩
+	 */
+	public double getGpa() {
+		if (grades.isEmpty()) {
+			return 0;
+		}
+		double points = 0;
+		for (Grade grade : grades) {
+			points = gradePointsFor(grade);
+		}
+		if(isHornor)
+			points+=1;
+		return points /grades.size();
+	}
+
+	private double gradePointsFor(Grade grade) {
+		switch (grade) {
+		case A:
+			return 4;
+		case B:
+			return 3;
+		case C:
+			return 2;
+		case D:
+			return 1;
+		default:
+			return 0;
+		}
 	}
 	
 }
